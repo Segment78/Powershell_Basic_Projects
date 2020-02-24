1a444f51e1832bf55ae2a157c5191d503fb0037b
Segment78 <61436241+Segment78@users.noreply.github.com>
2020-02-24 13:45:49 -0800

    Add files via upload
    Script that allows a user to input a number and receive the corresponding letter in the alphabet.

---
+++ abc_number.ps1
@@ -0,0 +1,10 @@
+﻿#ABC-Number by Chris Sanders
+#Creates function to provide corresponding alphabet letter to number input.
+function ABC-Number{
+                    Param(
+                          [Parameter(Mandatory=$true)]
+                          [string] $Letter_Number?
+                          )
+$alphabet = 'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'
+Write-host 'Letter', $Letter_Number?, 'is' $alphabet[($Letter_Number?-1)]
+                    }
