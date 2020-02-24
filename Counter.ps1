ab11cf0287434f1ac9453f00c566e1b6648e9094
Segment78 <61436241+Segment78@users.noreply.github.com>
2020-02-24 13:48:36 -0800

    Add files via upload
    Allows user to input an interval and maximum. Output will be a list of numbers counted to the requested maximum in the interval requested as well as each number's binary, octal, and hexadecimal conversions.

---
+++ Counter.ps1
@@ -0,0 +1,25 @@
+﻿#Counter function by Chris Sanders
+#Counts by desired interval to specified number, converts to binary, octal, and hexadecimal equivalent and outputs resultant object as a table
+Function Count{
+                Param(
+                [Parameter(Mandatory=$true)]
+                [int32]$Number_to_count_by?,
+                [Parameter(Mandatory=$true)]
+                [int32]$Number_to_count_to?
+                )
+                $x=0
+                while($x -le ($Number_to_count_to?)){
+                $Binary = [convert]::ToString($x,2)
+                $Octal = [convert]::ToString($x,8)
+                $Hexadecimal = [convert]::ToString($x,16)
+
+                $obj = New-Object psobject -Property @{`
+                        "Decimal" = $x;
+                        "Binary" = $Binary;
+                        "Octal" = $Octal;
+                        "Hex" = $Hexadecimal}
+                $obj | select Decimal,Binary,Octal,Hex
+
+                $x = ($x + $Number_to_count_by?)
+                }
+                }
