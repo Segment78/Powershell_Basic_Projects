5fa91851adf2225fa82c0478df124c2c19daa1b8
Segment78 <61436241+Segment78@users.noreply.github.com>
2020-02-24 13:43:58 -0800

    Add files via upload
    Simple script to count in a Fibonacci sequence to a desired maximum.

---
+++ Get_Fib.ps1
@@ -0,0 +1,16 @@
+﻿Function Get-Fib{
+    Param(
+    [Parameter(Mandatory=$true)]
+    [int64] $What_should_we_count_to?
+        )
+    $x = 1
+    $y = 1
+    $i = 1
+    While($x -lt $What_should_we_count_to?){
+        "Iteration $i is $x"
+        $z = $x + $y
+        $x = $y
+        $y = $z
+        $i = $i + 1
+                                            }
+                }
